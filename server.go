aa1ebc4043ba03c40b674f482ea16c7058c6d209
Mahesh Yadav <mahesh.iitmandi@gmail.com>
2025-03-26 08:43:19 +0530

    writing a custom handler function

--- server.go
+++ server.go
@@ -2,14 +2,16 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func RootHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Welcome to the Go Gin API!",
+	})
+}
+
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello, World!",
-		})
-	})
+	router.GET("/", RootHandler)
 
 	router.Run(":3000")
 }
